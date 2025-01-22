4837c8e0ca2e7c897cd4d7c88ef6a20c9dc74c74
jonathan <johadamas13@gmail.com>
2025-01-22 15:21:49 +0700

    first commit

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "subscription_id" {
+  description = "The Azure subscription ID."
+  type        = string
+}
+
+variable "user_email" {
+  description = "Email of the Databricks user."
+  type        = string
+}
+
+# variable "service_principal_object_id" {
+#   description = "The object ID of the service principal"
+#   type        = string
+# }
